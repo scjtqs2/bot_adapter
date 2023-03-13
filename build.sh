e313bf1eedab5578345b3d55bf1d8f7ae30f4d19
scjtqs <scjtqs@qq.com>
2023-03-13 11:49:35 +0800

    v1

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker buildx create --use --name bot_adapter
-docker buildx build --tag scjtqs/bot_adapter:latest --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7 --push  .
+docker buildx build --tag scjtqs/bot_adapter:v1 --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7 --push  .
 docker buildx rm bot_adapter
